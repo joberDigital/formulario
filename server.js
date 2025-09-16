d78d31bbd6efb72bcd51c612a603e6d831d70c7f
jhon fredy <joberdigital6@gmail.com>
2025-09-15 19:47:11 -0500

    feat: Add GitHub Actions CI/CD workflow

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const correoEstatico = 'micorreo@gmail.com';
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la página de contacto!');
+});
+
+app.post('/enviar-mensaje', (req, res) => {
+  const { nombre, email, mensaje } = req.body;
+
+  if (nombre === '' || email === '' || mensaje === '') {
+    return res.status(400).json({ error: 'Por favor, complete todos los campos.' });
+  }
+
+  const mensajeMail = `Nombre: ${nombre}\nCorreo electrónico: ${email}\nMensaje: ${mensaje}`;
+
+  // Enviar mensaje por correo electrónico
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // or 'STARTTLS'
+  });
+
+  transporter.sendMail({
+    from: correoEstatico,
+    to: correoEstatico,
+    subject: 'Mensaje desde sitio web',
+    text: mensajeMail,
+  }, (error, info) => {
+    if (error) {
+      return res.status(500).json({ error: 'Error al enviar el mensaje.' });
+    }
+    res.json({ mensaje: 'Mensaje enviado con éxito!' });
+  });
+});
+
+
+app.get('/contacto', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
