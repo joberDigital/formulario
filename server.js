72f41601826f971e98bdf5d0598041e48956e732
jhon fredy <joberdigital6@gmail.com>
2025-09-15 20:38:33 -0500

    feat: Add GitHub Actions CI/CD workflow

--- server.js
+++ server.js
@@ -5,50 +5,14 @@ const correoEstatico = 'micorreo@gmail.com';
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Bienvenido a la página de contacto!');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/formulario/', (req, res) => {
-  const { nombre, email, mensaje } = req.body;
-
-  if (nombre === '' || email === '' || mensaje === '') {
-    return res.status(400).json({ error: 'Por favor, complete todos los campos.' });
-  }
-
-  const mensajeMail = `Nombre: ${nombre}\nCorreo electrónico: ${email}\nMensaje: ${mensaje}`;
-
-  // Enviar mensaje por correo electrónico
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // or 'STARTTLS'
-  });
-
-  transporter.sendMail({
-    from: correoEstatico,
-    to: correoEstatico,
-    subject: 'Mensaje desde sitio web',
-    text: mensajeMail,
-  }, (error, info) => {
-    if (error) {
-      return res.status(500).json({ error: 'Error al enviar el mensaje.' });
-    }
-    res.json({ mensaje: 'Mensaje enviado con éxito!' });
-  });
-});
-
-
-app.get('/formulario', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-
-// app.post('/contacto', (req, res) => {
-//   const { nombre, email, mensaje } = req.body;
-
-//   // Resto del código para enviar el mensaje por correo electrónico
+// app.post('/formulario/', (req, res) => {
+//   // Código para manejar la solicitud POST en la ruta "/formulario/"
 // });
 
-  
 app.listen(3000, () => {
   console.log('Servidor escuchando en el puerto 3000');
 });
+
